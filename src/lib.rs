c05723dbe5b08dc760b1d98d14031f9138ea1153
aobatact <aobatact144@gmail.com>
2021-04-01 11:18:36 +0900

    display

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub struct UtcZst<const HOUR: i32, const MINUTE: u32>;
 impl<const HOUR: i32, const MINUTE: u32> UtcZst<HOUR, MINUTE> {
     pub const OFFSET_SECS: i32 =
         HOUR * HOUR_TO_SEC + if HOUR < 0 { -1 } else { 1 } * (MINUTE as i32) * MIN_TO_SEC;
+    pub const IS_IN_VALID_RANGE: bool = (HOUR >= -12) & (HOUR <= 14) & (MINUTE < 60);
 }
 impl<const HOUR: i32, const MINUTE: u32> Offset for UtcZst<HOUR, MINUTE> {
     fn fix(&self) -> FixedOffset {
@@ -32,6 +33,11 @@ impl<const HOUR: i32, const MINUTE: u32> TimeZone for UtcZst<HOUR, MINUTE> {
         *self
     }
 }
+impl<const HOUR: i32, const MINUTE: u32> core::fmt::Display for UtcZst<HOUR, MINUTE> {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "{:+03}:{:02}", HOUR, MINUTE)
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -41,12 +47,14 @@ mod tests {
     #[test]
     fn it_works() {
         let p9 = UtcP9::default();
+        assert_eq!(&p9.to_string(), "+09:00");
         let n = p9.ymd(2000, 1, 1).and_hms(12, 00, 00);
         assert_eq!(
             n.naive_utc(),
             NaiveDate::from_ymd(2000, 1, 1).and_hms(3, 0, 0)
         );
         let m9 = UtcM9::default();
+        assert_eq!(&m9.to_string(), "-09:00");
         let n = m9.ymd(2000, 1, 1).and_hms(12, 00, 00);
         assert_eq!(
             n.naive_utc(),
